b1a2445db89994505a6c015e1784eb295eb66b46
Kataoka Yuto <konamonhyogo@gmail.com>
2022-04-17 19:48:09 +0900

    レンダリングの最適化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import React, { useState, useEffect, memo } from 'react';
 
 const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  console.log('レンダリングしてるよ');
+  return (
+    <>
+      <div>レンダリングの確認</div>
+      <Child />
+    </>
+  );
 };
 
 export default Home;
+
+/*eslint-disable-line*/ const Child = memo(() => {
+  const [count, setCount] = useState(0);
+  const text: string[] = ['Zero', 'one', 'two', 'three'];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count === 3) {
+        return setCount(0);
+      }
+      setCount((i) => i + 1);
+    }, 1000);
+
+    return () => {
+      console.log(count);
+      clearTimeout(interval);
+    };
+  }, [count]);
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>{text[count]}</h1>
+    </>
+  );
+});
